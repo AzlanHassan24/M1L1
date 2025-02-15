917302bf0f040da6293c376fd0e012786f26d28e
AzlanHassan24 <s.masoodukpk@gmail.com>
2025-02-15 15:49:10 +0000

    Add files via upload

---
+++ L2Program.py
@@ -0,0 +1,23 @@
+print("hello world")
+
+#Print a sentence 
+print("hello my name is Hassan")
+
+print("welcome to the world of python")
+
+print("hello azlan is bad")
+
+#print a number
+
+print(78)
+
+print(5)
+
+#next line after print
+
+print("hello python \n")
+
+
+
+
+
